dbd5bab59019e31a0563819116ea18206d1c0702
Cevheri Bozoğlan <cevheri.bozoglan@aurorabilisim.com>
2020-09-29 15:34:10 +0300

    add scatter square module

---
+++ scatter_squares.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+
+x_values = list(range(1, 1001))
+y_values = [i ** 2 for i in x_values]
+
+plt.scatter(x_values,
+            y_values,
+            c=y_values,  # (0, 0, 0.8),  # c="red",
+            cmap=plt.cm.Blues,
+            edgecolors="none",
+            s=40)
+
+# Set chart title and label axes.
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+# Set size of tick labels.
+# plt.tick_params(axis="both", which="major", labelsize=14)
+
+# Set the range for each axis.
+plt.axis([0, 1100, 0, 1100000])
+
+plt.show()
+# plt.savefig("squares_plot.png", bbox_inches="tight")
